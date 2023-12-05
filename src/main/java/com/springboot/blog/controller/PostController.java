6909b691b6b1e107eb0a78b2272d1772449668ba
Thanakorn Rattanapornchai <thanakornrattanapornchai@gmail.com>
2023-12-05 16:50:56 +0700

    add post controller and create post

---
+++ src/main/java/com/springboot/blog/controller/PostController.java
@@ -0,0 +1,28 @@
+package com.springboot.blog.controller;
+
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+    private PostService postService;
+    // Autowire are not needed here because we are using constructor injection instead of field injection
+    // Autowire is not recommended because it makes the code tightly coupled
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create Post
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto) {
+//        return postService.createPost(postDto);
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+}
