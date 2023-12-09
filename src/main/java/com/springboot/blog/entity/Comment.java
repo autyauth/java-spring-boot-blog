d68cad10205cddf4b224ae3cf5efd443b451eb8e
Thanakorn Rattanapornchai <thanakornrattanapornchai@gmail.com>
2023-12-09 17:46:43 +0700

    add @Valid to CommentController and move Validtator comment to commentDto , and add validtor exception handler

--- src/main/java/com/springboot/blog/entity/Comment.java
+++ src/main/java/com/springboot/blog/entity/Comment.java
@@ -2,6 +2,7 @@ package com.springboot.blog.entity;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,16 +20,8 @@ public class Comment {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    // not empty
-    @NotEmpty(message = "Name cannot be empty")
     private String name;
-    // not empty
-    // valid email address
-    @NotEmpty(message = "Email cannot be empty")
-    @Email(message = "Email should be valid")
     private String email;
-    // not empty
-    @NotEmpty(message = "Body cannot be empty")
     private String body;
 
     @ManyToOne(fetch = FetchType.LAZY)
--- src/main/java/com/springboot/blog/payload/CommentDto.java
+++ src/main/java/com/springboot/blog/payload/CommentDto.java
@@ -1,11 +1,24 @@
 package com.springboot.blog.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CommentDto {
     private long id;
+    // not empty
+    // at least 2 characters
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 2, message = "Name must be at least 2 characters long")
     private String name;
+    // not empty
+    // valid email address
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
+    // not empty
+    @NotEmpty(message = "Body cannot be empty")
     private String body;
 }
