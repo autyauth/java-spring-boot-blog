425b2951bb193921ee8927fadb21a9bdbc25aaa2
Thanakorn Rattanapornchai <thanakornrattanapornchai@gmail.com>
2023-12-09 16:06:28 +0700

    refactor Post as i add comment to postDto

--- src/main/java/com/springboot/blog/payload/PostDto.java
+++ src/main/java/com/springboot/blog/payload/PostDto.java
@@ -2,11 +2,14 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data // This annotation is from Lombok, it generates getters and setters for the fields in this class when the project is compiled
 public class PostDto {
     private long id;
     private String title;
     private String description;
     private String content;
+    private Set<CommentDto> comments;
 
 }
